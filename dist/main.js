7da538b5ceb28ef4ad0a6e0b4b99735eef9ecc66
Sercan Ateş <sercanio@users.noreply.github.com>
2024-03-08 20:36:21 +0300

    Create files

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+"use strict";
+const user = {
+    name: 'Sercan',
+    id: 1,
+};
+console.log(user.name);
+function greet(person, date) {
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+greet(user.name, new Date());
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+interface User {
+  name: string;
+  id: number;
+}
+
+const user : User = {
+  name: 'Sercan',
+  id: 1,
+};
+
+console.log(user.name);
+
+function greet(person: string, date: Date) {
+  console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+
+greet(user.name, new Date());
